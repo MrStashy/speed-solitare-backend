1f20f6c69c7fd935bf6a5efa0ee88dd7933630e7
Will Baxter <bax.will@googlemail.com>
2024-10-22 11:06:00 +0100

    Save user and score end point

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,37 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class UserController extends Controller
 {
     public function getUsernames(): JsonResponse {
-        $usernames = User::pluck('username');
+        $usersWithScores = User::with(['scores' => function ($query) {
+            $query->orderBy('final_score', 'desc')->limit(5);
+        }])
+        ->take(10)
+        ->get();
 
-        return response()->json($usernames);
+        return response()->json($usersWithScores);
+    }
+
+    public function postUserAndScore(Request $request): JsonResponse {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string',
+            'finalScore' => 'required|string',
+        ]);
+    
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+    
+        $validatedData = $validator->validated();
+
+        $user = User::firstOrCreate(['username' => $validatedData['username']]);
+        $user->scores()->create(['final_score' => $validatedData['finalScore']]);
+
+        return response()->json(['message' => 'Data saved successfully']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/usernames', [UserController::class, 'getUsernames']);
+Route::post('/usernames', [UserController::class, 'postUserAndScore']);
