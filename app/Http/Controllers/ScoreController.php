49b346f4a7385d148a8e4017446d894888b4c6c2
Will Baxter <bax.will@googlemail.com>
2024-10-22 14:12:08 +0100

    Added top 10 scores route

---
+++ app/Http/Controllers/ScoreController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Score;
+
+use Illuminate\Http\JsonResponse;
+
+class ScoreController extends Controller
+{
+
+    public function getTopTen(): JsonResponse
+    {
+        $topScores = Score::with('user')
+            ->orderBy('final_score', 'desc')
+            ->take(10)
+            ->get(['final_score', 'user_id']);
+
+        return response()->json($topScores->map(function ($score) {
+            return [
+                'username' => $score->user->username, 
+                'score' => $score->final_score
+            ];
+        }));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ScoreController;
 
 Route::get('/usernames', [UserController::class, 'getUsernames']);
 Route::post('/usernames', [UserController::class, 'postUserAndScore']);
+Route::get('/scores', [ScoreController::class, 'getTopTen']);
